414f583332c8dd98101ae82e6ab6676f08a76fa5
enzoaugusto <62678687+enzoaugusto@users.noreply.github.com>
2020-06-22 02:09:47 -0300

    Arquivos Site

---
+++ script.js
@@ -0,0 +1,20 @@
+$('a.smooth-scroll')
+.click(function(event) {
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function() {
+        var $target = $(target);
+      });
+    }
+  }
+});
